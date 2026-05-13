6061246f8c96447656cea02271b50033fdeda9ee
saurabhski <saurabhananth91@gmail.com>
2026-05-12 21:42:27 -0400

    Client and server scripts

---
+++ udp_client.py
@@ -0,0 +1,11 @@
+import socket
+
+target = "www.google.com"
+port = 80
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.sendto(b"AAABBBCCC", (target,port))
+data, addr = client.recvfrom(4096)
+
+print(data.decode())
+client.close()
